b5497c6b45aad4c8d3df21a932da3e40997c6eb6
Renan Guedes Silveira <renangsilveira@gmail.com>
2026-06-20 21:04:17 -0300

    fix: correct Avro Gradle plugin id

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("com.github.davidmc24.gradle.avro") version "1.9.1"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 	jacoco
 }
 
