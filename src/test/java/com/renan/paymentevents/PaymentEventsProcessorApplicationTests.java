e9745c315baf92539de47e4a25cc4700832f1b21
Renan Guedes Silveira <renangsilveira@gmail.com>
2026-06-20 21:21:02 -0300

    fix: exclude DataSource autoconfiguration from default context test

--- src/test/java/com/renan/paymentevents/PaymentEventsProcessorApplicationTests.java
+++ src/test/java/com/renan/paymentevents/PaymentEventsProcessorApplicationTests.java
@@ -1,11 +1,16 @@
 package com.renan.paymentevents;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 class PaymentEventsProcessorApplicationTests {
 
 	@Test
