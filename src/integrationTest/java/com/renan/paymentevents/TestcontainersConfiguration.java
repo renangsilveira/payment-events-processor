e78e0a7a552c520c55f43704da5ec6af0aca5611
Renan Guedes Silveira <renangsilveira@gmail.com>
2026-07-11 23:16:59 -0300

    fix: replace deprecated DynamicPropertyRegistry injection with DynamicPropertyRegistrar bean

--- src/integrationTest/java/com/renan/paymentevents/TestcontainersConfiguration.java
+++ src/integrationTest/java/com/renan/paymentevents/TestcontainersConfiguration.java
@@ -3,13 +3,15 @@ package com.renan.paymentevents;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
@@ -36,22 +38,28 @@ public class TestcontainersConfiguration {
 	@Bean
 	public GenericContainer<?> schemaRegistryContainer(
 			KafkaContainer kafkaContainer,
-			Network testNetwork,
-			DynamicPropertyRegistry registry) {
-		GenericContainer<?> schemaRegistry = new GenericContainer<>(
+			Network testNetwork) {
+		return new GenericContainer<>(
 				DockerImageName.parse("confluentinc/cp-schema-registry:7.7.1"))
 				.withNetwork(testNetwork)
 				.withExposedPorts(8081)
 				.withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
 				.withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
-				.withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
-						"PLAINTEXT://kafka:9092")
-				.waitingFor(Wait.forHttp("/subjects").forStatusCode(200))
+				.withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", "PLAINTEXT://kafka:9092")
+				.withEnv("SCHEMA_REGISTRY_KAFKASTORE_SECURITY_PROTOCOL", "PLAINTEXT")
+				.waitingFor(Wait.forHttp("/subjects")
+						.forStatusCode(200)
+						.withStartupTimeout(Duration.ofSeconds(180)))
 				.dependsOn(kafkaContainer);
+	}
 
-		registry.add("spring.kafka.properties.schema.registry.url",
-				() -> "http://" + schemaRegistry.getHost() + ":" + schemaRegistry.getMappedPort(8081));
-
-		return schemaRegistry;
+	@Bean
+	public DynamicPropertyRegistrar schemaRegistryProperties(
+			GenericContainer<?> schemaRegistryContainer) {
+		return registry -> registry.add(
+				"spring.kafka.properties.schema.registry.url",
+				() -> "http://" + schemaRegistryContainer.getHost()
+						+ ":" + schemaRegistryContainer.getMappedPort(8081)
+		);
 	}
 }
